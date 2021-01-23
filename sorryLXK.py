e75217c67e7200e9cc175622fde665c90bbd8dd2
Vidocq <949867756@qq.com>
2021-01-23 23:23:31 +0800

    Finish script

--- sorryLXK.py
+++ sorryLXK.py
@@ -6,44 +6,20 @@ fileDir = "./"
 fileList = []
 for root, dirs, files in os.walk(fileDir):
     for fileObj in files:
-        if '.git' in root or 'icon' in root:
+        if '.git' in root or 'icon' in root or 'docker' in root or 'Loon' in root or 'QuantumultX' in root or 'Surge' in root:
             continue
         fileList.append(os.path.join(root, fileObj))
 
+restr = r"JSON\.stringify\(process\.env\)\.indexOf\(['\"]GITHUB['\"]\)>-1"
 for fileObj in fileList:
     if 'sorryLXK' in fileObj:
         continue
-    f = open(fileObj, 'r+', errors='ignore')
+    f = open(fileObj, 'r', errors='ignore')
     lines = f.readlines()
-    f.seek(0)
-    # f.truncate()
-    for line in lines:
-        origin = 'JSON.stringify(process.env).indexOf("GITHUB")>-1'
-        origin2 = "JSON.stringify(process.env).indexOf('GITHUB')>-1"
-        result = '0'
-        f.write(line.replace(origin, result))
     f.close()
-
-fileDir = "./"
-fileList = []
-for root, dirs, files in os.walk(fileDir):
-    for fileObj in files:
-        if '.git' in root or 'icon' in root:
-            continue
-        fileList.append(os.path.join(root, fileObj))
-
-for fileObj in fileList:
-    if 'sorryLXK' in fileObj:
-        continue
-    f = open(fileObj, 'r+', errors='ignore')
-    lines = f.readlines()
-    f.seek(0)
-    # f.truncate()
+    f = open(fileObj, 'w')
     for line in lines:
-        origin = 'JSON.stringify(process.env).indexOf("GITHUB")>-1'
-        origin2 = "JSON.stringify(process.env).indexOf('GITHUB')>-1"
         result = '0'
-        f.write(line.replace(origin2, result))
+        line = re.sub(restr, result, line)
+        f.write(line)
     f.close()
-
-    # sed -i s/JSON.stringify(process.env).indexOf("GITHUB")>-1/0/g `grep JSON.stringify(process.env).indexOf("GITHUB")>-1 -rl --include="*.*" ./`
