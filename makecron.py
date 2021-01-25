3046094ac4e932c5f887e37a734a27cadb3a6fd9
Vidocq <949867756@qq.com>
2021-01-26 01:57:23 +0800

    edit actions

--- makecron.py
+++ makecron.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from string import Template
+from crontzconvert import convert as convert_cron
 
 crontab_URL = "https://github.com/VidocqH/jd_scripts/raw/master/docker/crontab_list.sh"
 r = requests.get(crontab_URL)
@@ -22,47 +23,11 @@ for i in rarr[5:]:
         i = i.rstrip()
         iarr = i.split(" ")
         # Normalize GMT+8 to GMT+0
-        hourCron = iarr[1]
-        if '/' in iarr[1]:
-            hourCronPrefix = hourCron[:hourCron.find('/')]
-            hourCronExtend = hourCron[hourCron.find('/'):]
-        else:
-            hourCronPrefix = hourCron
-            hourCronExtend = ''
-        if hourCronPrefix != '*':
-            if ',' in hourCronPrefix: # 点只需排序
-                hourArr = hourCronPrefix.split(',')
-                temIntArr = []
-                for i in hourArr:
-                    temIntArr.append(int(int(i) + 16) % 24)
-                hourArr = sorted(temIntArr)
-                hourArr = [str(i) for i in hourArr]
-                iarr[1] = ','.join(hourArr) + hourCronExtend
-            elif '-' in hourCronPrefix: # 区间需要补充
-                hourArr = hourCronPrefix.split('-')
-                temIntArr = []
-                for i in hourArr:
-                    temIntArr.append(int(int(i) + 16) % 24)
-                if temIntArr[0] > temIntArr[1]:
-                    tem = temIntArr[0]
-                    temIntArr[0] = 0
-                    hourArr = temIntArr
-                    hourArr = [str(i) for i in hourArr]
-                    hourArr = '-'.join(hourArr)
-                    while tem != 24:
-                        hourArr += (',' + str(tem))
-                        tem += 1
-                    iarr[1] = hourArr + hourCronExtend
-                else:
-                    hourArr = temIntArr
-                    hourArr = [str(i) for i in hourArr]
-                    iarr[1] = '-'.join(hourArr) + hourCronExtend
-            else:
-                iarr[1] = str((int(iarr[1]) + 16) % 24)
         # 0-4元素为cron
         # 5-6元素为脚本执行命令
         cron = ' '.join(iarr[:5])
         comm = ' '.join(iarr[5:])
+        cron = convert_cron(cron, 'Asia/Shanghai', 'Europe/London')
         res[count].append(cron)
         res[count].append(comm)
         count += 1
@@ -74,7 +39,6 @@ for i in rarr[5:]:
 # convert array to dict
 crondic = []
 for i in res:
-    print(i)
     if i[0] == "签到": # 懒得处理的特别情况
         crondic.append({
             'nameCN': i[0],
